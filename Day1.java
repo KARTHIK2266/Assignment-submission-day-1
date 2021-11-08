21cfc835fe4224c1d1856f843e41c257e9b66cfb
KARTHIK2266 <93930752+KARTHIK2266@users.noreply.github.com>
2021-11-08 21:35:29 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello word..!");
+		System.out.println("My Name is: G Karthik");
+		System.out.println("I'm learning Java Zero to Hero @Lets Upgrade");
+	
+
+	}
+
+}
